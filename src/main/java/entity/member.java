a0db49c28c8b0eecd5e5d6fc72d3f22ba3062f3e
cppmonk <2682836079@qq.com>
2023-06-19 09:24:33 +0900

    第一天代码增量

---
+++ src/main/java/entity/member.java
@@ -0,0 +1,50 @@
+package entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Event> events;
+
+    public Member() {
+    }
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    // Getter和Setter方法
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+}
